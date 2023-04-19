1376e7a05e6228a1fb704f6b843a750cbf97d77b
Manfaily roland yao <manfailyrolandyao@gmail.com>
2023-04-19 14:09:18 +0000

    creation de la page d'acceuil index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+    <title>BOYOKA</title>
+<!--
+
+TemplateMo 548 Training Studio
+
+https://templatemo.com/tm-548-training-studio
+
+-->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/bootstrap.min.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/font-awesome.css')}}">
+
+    <link rel="stylesheet" href="{{asset('/css/templatemo-training-studio.css')}}">
+
+    </head>
+
+    <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="js-preloader" class="js-preloader">
+      <div class="preloader-inner">
+        <span class="dot"></span>
+        <div class="dots">
+          <span></span>
+          <span></span>
+          <span></span>
+
+        </div>
+      </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav ">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="#" class="logo">BOY<em>OKA</em></a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#features">About</a></li>
+                            <!-- <li class="scroll-to-section"><a href="#our-classes">Classes</a></li>
+                            <li class="scroll-to-section"><a href="#sc,hedule">Schedules</a></li> -->
+                            <li class="scroll-to-section"><a href="#trainers">Catalog</a></li>
+                            <li class="scroll-to-section"><a href="#contact-us">Contact</a></li>
+
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner">
+        <video autoplay muted loop id="bg-video">
+            <source src="{{asset('/images/boss.mp4')}}" type="video/mp4" />
+        </video>
+
+        <div class="video-overlay header-text">
+            <div class="caption">
+
+                <h2>BOYOKA<em></em></h2>
+                <div class="main-button scroll-to-section">
+                    <a href="#">AUDIO & VIDEO DISTRIBUTION – RIGHTS MANAGEMENT – PUBLISHING – NEWS – DSP</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** Features Item Start ***** -->
+    <section class="section" id="features">
+        <div class="container">
+            <div class="row">
+                <!-- class="col-lg-6 offset-lg-3"  -->
+                <div >
+                    <div class="section-heading">
+                        <h2>WHO IS BO<em>YOKA</em></h2>
+                        <img src="{{asset('/images/line-dec.png')}}" alt="waves">
+                        <p style="font-size: 22px !important;"><big>Boyoka is a cutting-edge digital distributor that
+                            specializes in audio and video distribution, music publishing, and right management. The company has established itself as a reliable and trustworthy platform for independent artists and labels looking to get their music out to a wider audience. With a user-friendly interface and a vast network of distribution partners, Boyoka makes it easy for musicians to distribute and monetize their music across different platforms and streaming services. The company also offers a range of services related to music publishing,
+                             such as royalty collection, licensing, and copyright management. With its innovative approach and
+                            commitment to supporting independent artists, Boyoka has quickly become a go-to platform for musicians looking to take their careers to the next level.</big></p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- ***** Testimonials Starts ***** -->
+    <section class="section" id="trainers">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>CATA<em>LOG</em></h2>
+                        <img src="{{asset('/images/line-dec.png')}}" alt="">
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/zq5EAugWlQg">
+                        <div class="image-thumb">
+                            <img src="{{asset('/images/yababoss.jpg')}}" alt="">
+                        </div>
+                        <div class="down-content">
+                            <h4 >Teddy Benzo Ft Poison Mobutu</h4>
+                             <p>DANSE YA BA BOSS</p>
+                        </div>
+                    </a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/mGOjXCUcc4Q"><div class="image-thumb">
+                            <img src="{{asset('/images/GOLA.jpg')}}" alt="">
+                        </div>
+                        </a>
+                        <div class="down-content">
+                            <h4>Ferre Gola </h4>
+                            <p>Bizorbi</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/eqmmlWie6N8">
+                        <div class="image-thumb">
+                            <img src="{{asset('/images/tyger.jpg')}}" alt="">
+                        </div>
+                        <div class="down-content">
+                            <h4>Big Tyger</h4>
+                            <p>Na kangami</p>
+                        </div>
+                    </a>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/5m7mOdqb5WY">
+                        <div class="image-thumb">
+                            <img src="{{asset('/images/ROUND2.jpg')}}" alt="">
+                        </div>
+                        <div class="down-content">
+
+                            <h4>Laila and the groove</h4>
+                            <p>Round 2</p>
+
+                        </div>
+                    </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/EK4HyJelsnI">
+                        <div class="image-thumb">
+                            <img src="{{asset('/images/tyger.jpg')}}" alt="">
+                        </div>
+                        <div class="down-content">
+
+                            <h4>Big Tyger</h4>
+                            <p>Crush</p>
+
+                        </div>
+                    </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/CXvorpEpjcQ">
+                        <div class="image-thumb">
+                            <img src="https://www.eventsrdc.com/wp-content/uploads/2022/10/Laila-x-Makhalba-Malecheck-696x365.jpg" alt="">
+                        </div>
+                        <div class="down-content">
+
+                            <h4>Laila and the groove ft Makhalba Malecheck </h4>
+                            <p>MBM</p>
+
+                        </div>
+                    </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="https://youtu.be/w_ziSoKy-fw">
+                        <div class="image-thumb">
+                            <img src="{{asset('/images/nest.jpg')}}" alt="">
+                        </div>
+                        <div class="down-content">
+
+                            <h4>Nestelia forest </h4>
+                            <p>MBM</p>
+
+                        </div>
+                    </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="trainer-item">
+                        <a href="ttps://youtu.be/O2ea-mm4F5c">
+                        <div class="image-thumb">
+                            <img src="{{asset('/images/tedd.jpg')}}" alt="">
+                        </div>
+                        <div class="down-content">
+                            <h6 style="color: black!important;">Teddy Benzo feat. Bioman Killavib  Zuko Yadeblé -Spinho Stayze</h6>
+                            <p>bala bala Hood</p>
+
+                        </div>
+                    </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- ***** Testimonials Ends ***** -->
+
+    <!-- ***** Contact Us Area Starts ***** -->
+    <section id="contact-us" class="contact">
+        <div class="container" data-aos="fade-up">
+
+          <div class="section-title">
+            <h2>Contact</h2>
+            <p>Contact Us</p>
+          </div>
+
+          <div>
+            <iframe style="border:0; width: 100%; height: 270px;"  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d342.0216627259749!2d-75.68280035429724!3d39.66656983339181!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c700cc214115ed%3A0x2aa8e2ebc734c98c!2sChapman%20Rd%2C%20Delaware%2C%20%C3%89tats-Unis!5e0!3m2!1sfr!2sci!4v1680347178654!5m2!1sfr!2sci" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
+          <div class="row mt-5">
+
+            <div class="col-lg-4">
+              <div class="info">
+                <div class="address">
+                  <i class="bi bi-geo-alt"></i>
+                  <h4>Location:</h4>
+                  <p>I254 Chapman Rd, Ste 208 #11160
+                      Newark, Delaware 19702 Us</p>
+                </div>
+
+                <div class="email">
+                  <i class="bi bi-envelope"></i>
+                  <h4>Email:</h4>
+                  <p>infos@boyoka.com</p>
+                </div>
+
+                <div class="phone">
+                  <i class="bi bi-phone"></i>
+                  <h4>Call:</h4>
+                  <p>+1 (240) 406-9702</p>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </section><!-- End Contact Section-->
+    <!-- ***** Contact Us Area Ends ***** -->
+
+    <!-- ***** Footer Start ***** -->
+    <footer >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>© Copyright &copy; 2023 Boyoka
+
+                    -<a rel="nofollow" href="" class="tm-text-link" target="_parent"> All Rights Reserved.</a><br>
+
+
+                </p>
+
+                    <!-- You shall support us a little via PayPal to info@templatemo.com -->
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="{{asset('/js/jquery-2.1.0.min.js')}}"></script>
+
+    <!-- Bootstrap -->
+    <script src="{{asset('/js/popper.js')}}"></script>
+    <script src="{{asset('/js/bootstrap.min.js')}}"></script>
+
+    <!-- Plugins -->
+    <script src="{{asset('/js/scrollreveal.min.js')}}"></script>
+    <script src="{{asset('/js/waypoints.min.js')}}"></script>
+    <script src="{{asset('/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('/js/imgfix.min.js')}}"></script>
+    <script src="{{asset('/js/mixitup.js')}}"></script>
+    <script src="{{asset('/js/accordions.js')}}"></script>
+    <script>
+        setTimeout(function(){
+            location.reload();
+        }, 120000); // rafraîchit la page toutes les 5 secondes
+    </script>
+
+    <!-- Global Init -->
+    <script src="{{asset('/js/custom.js')}}"></script>
+
+  </body>
+</html>
